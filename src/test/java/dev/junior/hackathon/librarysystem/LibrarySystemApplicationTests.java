51402cf6fbc2c4d25b9bf0af01d8ed6bd6439d57
Akbar Amanov <qwertykelnmiir@gmail.com>
2022-12-13 18:52:53 +0600

    first commit

---
+++ src/test/java/dev/junior/hackathon/librarysystem/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.junior.hackathon.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
